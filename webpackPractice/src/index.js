e55f71041fdf3275826b4bac35cf47b13eade684
Sohil <sohilnaidoo@gmail.com>
2023-05-24 10:36:54 +0200

    Add part 7 and weppackPractice

---
+++ webpackPractice/src/index.js
@@ -0,0 +1,8 @@
+import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import 'core-js/stable/index.js'
+import 'regenerator-runtime/runtime.js'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
